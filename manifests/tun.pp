702f66596bb942092717427cf73b3b422c5c6015
Phil Fenstermacher <pcfens@wm.edu>
2016-11-30 23:52:11 -0500

    Add timeout options to tunnel settings

--- manifests/tun.pp
+++ manifests/tun.pp
@@ -72,6 +72,18 @@
 #   By default we look this value up in a stunnel::data class, which has a
 #   list of common answers.
 #
+# [*timeout_busy*]
+#   Time to wait for expected data
+#
+# [*timeout_close*]
+#    Time to wait for close_notify (set to 0 for buggy MSIE)
+#
+# [*timeout_connect*]
+#   Time to wait to connect to a remote host
+#
+# [*timeout_idle*]
+#   Time to keep an idle connection
+#
 # === Examples
 #
 #   stunnel::tun { 'rsyncd':
@@ -107,11 +119,15 @@ define stunnel::tun(
     $client,
     $accept,
     $connect,
-    $ssl_version = 'TLSv1',
-    $pid_file    = "/${name}.pid",
-    $debug_level = '0',
-    $log_dest    = "/var/log/${name}.log",
-    $conf_dir    = $stunnel::params::conf_dir
+    $timeout_busy    = undef,
+    $timeout_close   = undef,
+    $timeout_connect = undef,
+    $timeout_idle    = undef,
+    $ssl_version     = 'TLSv1',
+    $pid_file        = "/${name}.pid",
+    $debug_level     = '0',
+    $log_dest        = "/var/log/${name}.log",
+    $conf_dir        = $stunnel::params::conf_dir
 ) {
 
   $ssl_version_real = $ssl_version ? {
